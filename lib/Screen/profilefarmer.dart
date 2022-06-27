7f21277fc711b90ea8f95884a382f90e1826b8d2
inthuuu <ploy.mark1122@gmail.com>
2022-06-27 12:54:58 +0700

    ...

--- lib/Screen/profilefarmer.dart
+++ lib/Screen/profilefarmer.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:drone_for_smart_farming/Screen/login.dart';
-import 'package:drone_for_smart_farming/Screen/selectService.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
